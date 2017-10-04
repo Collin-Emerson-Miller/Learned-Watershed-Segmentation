fef47e48662ee36e0ad3bdb5aabac9983eec94d1
MaxConners <MaxConers@gmail.com>
2017-10-04 15:42:51 -0700

    "Adding testing"

---
+++ testing/test.py
@@ -0,0 +1,10 @@
+# Crafted by Collin Miller
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cremi
+from segmentation import learnedwatershed as lw
+
+segmentation
