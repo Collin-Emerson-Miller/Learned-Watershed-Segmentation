Fixed bug with root error edge calculation

segmentation/utils/graph_utils.py
@@ -253,8 +253,7 @@ def find_root_edge(shortest_path,
     ground_truth_seed = ground_truth_path[0]
 
     # Compute the root edge to increase (p(w)).
-    root_missing_cut_edge = find_missing_cut(shortest_path, ground_truth_cuts,
-                                             cut_edges)
+    root_missing_cut_edge = find_missing_cut(shortest_path, ground_truth_cuts)
 
     # Increment the number of children for the root edge.
     try:
@@ -316,7 +315,7 @@ def find_first_false_cut(ground_truth_path, ground_truth_cuts, cut_edges):
 # In[ ]:
 
 
-def find_missing_cut(shortest_path, ground_truth_cuts, cut_edges):
+def find_missing_cut(shortest_path, ground_truth_cuts):
     """
     Computes the root error missing cut of a shortest path.
 
@@ -326,22 +325,16 @@ def find_missing_cut(shortest_path, ground_truth_cuts, cut_edges):
     Args:
         shortest_path (list): The list of edges in the shortest path.
         ground_truth_cuts (list): The list ground truth cuts for the ground truth segmentation.
-        cut_edges (list): The list of cut edges from the current segmentation.
 
     Returns:
         tuple: The first erroneous cut edge in the shortest path.
     """
 
-    for i, node in enumerate(shortest_path):
-        try:
-            edge = (shortest_path[i], shortest_path[i + 1])
-            if edge in ground_truth_cuts or tuple(reversed(edge)) in ground_truth_cuts:
-                if edge not in cut_edges and tuple(reversed(edge)) not in cut_edges:
-                    return edge
-
-        except IndexError:
-            print("No Missing Cut.")
-            continue
+    for i, node in enumerate(xrange(len(shortest_path) - 1)):
+        u = shortest_path[i]
+        v = shortest_path[i+1]
+        if (u, v) in ground_truth_cuts or (v, u) in ground_truth_cuts:
+                return (u, v)
 
 def find_deviation(ground_truth_path, shortest_path):
     """
@@ -361,31 +354,6 @@ def find_deviation(ground_truth_path, shortest_path):
             return (ground_truth_path[i - 1], ground_truth_path[i])
     else:
         raise ValueError('No deviation.')
-
-
-# In[ ]:
-
-
-def generate_gt_cuts(gt_image, seeds, assignments=False):
-    graph = prims_initialize(gt_image)
-
-    for (x, y), d in np.ndenumerate(gt_image):
-        graph.node[(x, y)]['altitude'] = d
-
-    graph = minimum_spanning_forest(gt_image, graph, seeds)
-
-    cuts = get_cut_edges(graph)
-
-    if assignments:
-        for (x, y), d in np.ndenumerate(gt_image):
-            graph.node[(x, y)]['altitude'] = d
-
-        gt_assignments = nx.get_node_attributes(graph, 'seed')
-
-        return cuts, graph, gt_assignments
-    else:
-        return cuts
-
 
 # In[ ]:
 

segmentation/utils/prediction_utils.py
@@ -50,18 +50,18 @@ def input_generator(bach, train_path, input_path, tag):
                                         image_path,
                                         gt_path) 
         
-        seed_image = cv2.cvtColor(gt,cv2.COLOR_GRAY2RGB)
+        seed_image = gt
 
         for x, y in seeds:
             seed_image[x, y] = [255, 0, 0]
 
-        plt.imsave(os.path.join(foldername, "seed_image"), seed_image, cmap='gray')
+        plt.imsave(os.path.join(foldername, "seed_image.png"), seed_image, cmap='gray')
 
         bps = bach.boundary_probabilities(img)
         
         I_a = np.stack((img, bps), axis=-1)
 
-        plt.imsave(os.path.join(train_path, "bach", f_name), bps, cmap='gray')
+        plt.imsave(os.path.join(train_path, "bach", f_name + ".png"), bps, cmap='gray')
             
         yield f_name, img, bps, I_a, gt, gt_cuts, seeds
     

segmentation/utils/preprocessing_utils.py
@@ -10,6 +10,7 @@ import shutil
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from utils import display_utils
 
 
 def load_image(foldername, img_path, gt_path):
@@ -22,8 +23,7 @@ def load_image(foldername, img_path, gt_path):
     img = cv2.imread(img_path, 0)
     gt = cv2.imread(gt_path, 0)
     
-    plt.imsave(os.path.join(foldername, "img"), img, cmap='gray')
-    plt.imsave(os.path.join(foldername, "gt"), gt, cmap='gray')    
+    plt.imsave(os.path.join(foldername, "img.png"), img, cmap='gray')        
 
     seeds = generate_seeds(gt_path, foldername)
     
@@ -32,7 +32,11 @@ def load_image(foldername, img_path, gt_path):
     labels = cv2.imread(labels_path, 0)
     
     gt_cuts = get_gt_cuts(labels)
+
+    gt = display_utils.view_boundaries(np.zeros_like(img), gt_cuts)
     
+    plt.imsave(os.path.join(foldername, "gt.png"), gt, cmap='gray')
+
     return img, gt, gt_cuts, seeds
 
 
@@ -50,7 +54,7 @@ def generate_seeds(image_path, output_path):
     seed_path = os.path.join(output_path, "seeds.txt")
     labels_path = os.path.join(output_path, "labels.png")
     
-    os.system("gmic -v -1 " + image_path + " -negate -label_fg 0,0 -dilate_circ 6 -o " + labels_path + " -o -.asc | tail -n +2 | awk '{ for (i = 1; i<=NF; i++) {x[$i] += i; y[$i] += NR; n[$i]++; } } END { for (v in x) { if (v>0) print v,x[v]/n[v],y[v]/n[v] }}' > " + seed_path + "")
+    os.system("gmic -v -1 " + image_path + " -negate -channels 1 -label_fg 0,0 -dilate_circ 6 -o " + labels_path + " -o -.asc | tail -n +2 | awk '{ for (i = 1; i<=NF; i++) {x[$i] += i; y[$i] += NR; n[$i]++; } } END { for (v in x) { if (v>0) print v,x[v]/n[v],y[v]/n[v] }}' > " + seed_path + "")
 
     seeds = []
     f = open(output_path + "/seeds.txt", 'r')