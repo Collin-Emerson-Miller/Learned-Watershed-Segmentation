37e9e9dbf16d8d8f124b1e76f6cca87939c134e4
Collin Emerson Miller <Collin.Emerson.Miller@gmail.com>
2018-01-24 18:27:28 -0800

    Downloading all cremi data

--- segmentation/datasets/cremi.py
+++ segmentation/datasets/cremi.py
@@ -1,7 +1,40 @@
-cremi_urls = ["https://cremi.org/static/data/sample_A_20160501.hdf",
+import datasetutils
+import os
+import h5py
+
+
+cremi_train_urls = ["https://cremi.org/static/data/sample_A_20160501.hdf",
               "https://cremi.org/static/data/sample_B_20160501.hdf",
-              "https://cremi.org/static/data/sample_C_20160501.hdf",
-              "https://cremi.org/static/data/sample_A%2B_20160601.hdf",
+              "https://cremi.org/static/data/sample_C_20160501.hdf"]
+
+cremi_test_urls = ["https://cremi.org/static/data/sample_A%2B_20160601.hdf",
               "https://cremi.org/static/data/sample_B%2B_20160601.hdf",
               "https://cremi.org/static/data/sample_C%2B_20160601.hdf"]
 
+cremi_path = "cremi"
+
+def get_data(dataset=0):
+    """Returns the raw data and neuron ids.
+    
+    """
+    print(os.getcwd())
+
+    if not os.path.exists(cremi_path):
+        os.mkdir(cremi_path)
+    
+    url = cremi_train_urls[dataset]
+
+    file_name = os.path.join(cremi_path, url.split('/')[-1])
+
+    if not os.path.exists(file_name) or (datasetutils.get_url_file_size(url) != os.path.getsize(file_name)):
+        print("Downloading File")
+        datasetutils.download_file(url, file_name)
+
+    # Get neuron ids from Cremi data.
+    with h5py.File(file_name, "r") as hdf:
+        print ("Initializing...")
+        labels = hdf['volumes']['labels']['neuron_ids'][:]
+        raw = hdf['volumes']['raw'].value
+        print ("Done!")
+
+        return raw, labels
